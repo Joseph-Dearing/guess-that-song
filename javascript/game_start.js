144576749b96559e64e16a42b31722445533f595
Joseph Dearing <joe@Josephs-MacBook-Pro.local>
2018-11-26 14:11:36 -0500

    possible fix for autoplay policy on most web browsers such as Google Chrome

--- javascript/game_start.js
+++ javascript/game_start.js
@@ -1,5 +1,14 @@
 /*This is telling the page to play the linked audio file once the page is loaded */
-window.onload = function() {
-    document.getElementById("game_start").play();
-}
-
+// window.onload = function() {
+//     document.getElementById("game_start").play();
+// }
+//The code above has been grayed out as I am trying to find a way to over-ride Chrome's 
+//and other web browers auto-play blocking features.
+var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+    if(!isChrome){
+      $('#iframeAudio').remove()
+    }
+  else{
+     $('#playAudio').remove() //just to make sure that it will not have 2x audio in the background 
+  }
+//The code above is new, and might be able to over-ride Chromes autoplay block policy
