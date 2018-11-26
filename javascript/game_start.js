47dc145677e0fa43dbbc4f86822a13a9753decef
Joseph Dearing <joe@Josephs-MacBook-Pro.local>
2018-11-26 14:49:35 -0500

    This code fixed sound not playing on my website due to autoplay policys on most web browsers including Google Chrome

--- javascript/game_start.js
+++ javascript/game_start.js
@@ -2,13 +2,13 @@
 // window.onload = function() {
 //     document.getElementById("game_start").play();
 // }
-//The code above has been grayed out as I am trying to find a way to over-ride Chrome's 
-//and other web browers auto-play blocking features.
+//The code above has been grayed out as I am trying to find a way to over-ride Chrome's autoplay policy
 var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
     if(!isChrome){
       $('#iframeAudio').remove()
     }
+    //This code will over-ride Chromes auto-play blocking policy
   else{
      $('#playAudio').remove() //just to make sure that it will not have 2x audio in the background 
   }
-//The code above is new, and might be able to over-ride Chromes autoplay block policy
+//This code is for browsers other then Chrome and also will prevent double audio in the background
